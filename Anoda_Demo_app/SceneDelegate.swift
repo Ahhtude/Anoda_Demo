42ce416a931d8120bf30b6b8117f0037053f3b9f
Sergey <whteahhtude@gmail.com>
2020-07-13 18:41:55 +0300

    final

--- Anoda_Demo_app/SceneDelegate.swift
+++ Anoda_Demo_app/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
+        
         let baseTabBarController = BaseTabBarController()
 
         window.rootViewController = baseTabBarController
